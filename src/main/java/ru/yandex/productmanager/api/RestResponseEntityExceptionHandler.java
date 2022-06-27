fef6006cabf0ba76d5732e7f311ab57b148207da
nt1dc <nt1dcare@gmail.com>
2022-06-27 14:25:39 +0300

    fix

--- src/main/java/ru/yandex/productmanager/api/RestResponseEntityExceptionHandler.java
+++ src/main/java/ru/yandex/productmanager/api/RestResponseEntityExceptionHandler.java
@@ -1,12 +1,19 @@
 package ru.yandex.productmanager.api;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import ru.yandex.productmanager.dto.Error;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 @ControllerAdvice
@@ -27,5 +34,17 @@ public class RestResponseEntityExceptionHandler
             RuntimeException ex, WebRequest request) {
         return ResponseEntity.status(400).body(new Error(400, "Validation Failed"));
     }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request) {
+        Error apiError = new Error(400, "validation failed");
+        return handleExceptionInternal(
+                ex, apiError, headers, HttpStatus.valueOf(apiError.getCode()), request);
+    }
+
 
 }
